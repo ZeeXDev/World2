f4f6ef7f27454fccc08c41b0afd2e49885c86c6d
ZeeXDev <otakukingcey@gmail.com>
2025-12-30 07:01:44 +0000

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-Falseedit @VJ_Botz
+#Falseedit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
@@ -196,5 +196,6 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
